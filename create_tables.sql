a8eb25e0b5133d216841391cc692a76dfdb40677
Godwin <ekainugodwin@gmail.com>
2020-07-20 14:37:43 +0100

    added scriptlog table

--- create_tables.sql
+++ create_tables.sql
@@ -2,13 +2,16 @@ DROP TABLE IF EXISTS public.authors;
 DROP TABLE IF EXISTS public.comments;
 DROP TABLE IF EXISTS public.posts;
 DROP TABLE IF EXISTS public.reviews;
+DROP TABLE IF EXISTS public.comment_review;
+DROP TABLE IF EXISTS public.word_count;
+DROP TABLE IF EXISTS public.script_log;
 
 CREATE TABLE public.authors (
     author_id VARCHAR(256) PRIMARY KEY,
     author VARCHAR(256),
     meibi VARCHAR(256),
     meibx VARCHAR(256)
-)
+);
 
 CREATE TABLE public.comments (
     comment_id VARCHAR(256) PRIMARY KEY,
@@ -17,7 +20,7 @@ CREATE TABLE public.comments (
     author VARCHAR(256),
     date DATETIME,
     vote INTEGER
-)
+);
 
 CREATE TABLE public.posts (
     post_id VARCHAR(256) PRIMARY KEY,
@@ -29,14 +32,14 @@ CREATE TABLE public.posts (
     url VARCHAR(256),
     date DATETIME,
     number_of_retrieved_comments INTEGER
-)
+);
 
 CREATE TABLE public.word_count (
     author_id VARCHAR(256) PRIMARY KEY,
     author VARCHAR(256),
     word_count_stopwords DOUBLE PRECISION,
     word_count_nostopwords DOUBLE PRECISION
-)
+);
 
 CREATE TABLE public.comment_review (
     author_id VARCHAR(256) PRIMARY KEY,
@@ -47,4 +50,9 @@ CREATE TABLE public.comment_review (
     date DATETIME,
     content VARCHAR(256),
     sentiment VARCHAR(256)
-)
+);
+
+CREATE TABLE public.script_log (
+  script_name varchar(32) NOT NULL,
+  processed_file smallint
+);
