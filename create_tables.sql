be2d4dc58549e206fb9060b4a47412a7823e7b81
Godwin <ekainugodwin@gmail.com>
2020-07-26 21:52:29 +0100

    additional files

--- create_tables.sql
+++ create_tables.sql
@@ -1,12 +1,11 @@
-DROP TABLE IF EXISTS public.authors;
+DROP TABLE IF EXISTS public.author;
 DROP TABLE IF EXISTS public.comments;
 DROP TABLE IF EXISTS public.posts;
-DROP TABLE IF EXISTS public.reviews;
 DROP TABLE IF EXISTS public.comment_review;
 DROP TABLE IF EXISTS public.word_count;
 DROP TABLE IF EXISTS public.script_log;
 
-CREATE TABLE public.authors (
+CREATE TABLE public.author (
     author_id VARCHAR(256) PRIMARY KEY,
     author VARCHAR(256),
     meibi VARCHAR(256),
@@ -15,40 +14,39 @@ CREATE TABLE public.authors (
 
 CREATE TABLE public.comments (
     comment_id VARCHAR(256) PRIMARY KEY,
-    post_id VARCHAR(256),
-    content VARCHAR(256),
-    author VARCHAR(256),
-    date DATETIME,
-    vote INTEGER
+    post_id VARCHAR(65000),
+    content VARCHAR(65535),
+    author VARCHAR(65000),
+    date VARCHAR(65000),
+    vote VARCHAR(65000)
 );
 
 CREATE TABLE public.posts (
     post_id VARCHAR(256) PRIMARY KEY,
-    title VARCHAR(256),
-    blogger_name VARCHAR(256),
-    blogger_id VARCHAR(256),
-    number_of_comments INTEGER,
-    content VARCHAR(256),
-    url VARCHAR(256),
-    date DATETIME,
-    number_of_retrieved_comments INTEGER
+    title VARCHAR(65000),
+    blogger_name VARCHAR(65000),
+    blogger_id VARCHAR(65000),
+    number_of_comments VARCHAR(256),
+    content VARCHAR(65535),
+    url VARCHAR(65000),
+    date VARCHAR(65000),
+    number_of_retrieved_comments VARCHAR(5000)
 );
 
 CREATE TABLE public.word_count (
     author_id VARCHAR(256) PRIMARY KEY,
-    author VARCHAR(256),
-    word_count_stopwords DOUBLE PRECISION,
-    word_count_nostopwords DOUBLE PRECISION
+    author VARCHAR(1000),
+    word_count_stopwords VARCHAR,
+    word_count_nostopwords VARCHAR
 );
 
 CREATE TABLE public.comment_review (
     author_id VARCHAR(256) PRIMARY KEY,
-    author VARCHAR(256),
+    author VARCHAR(1000),
     post_id VARCHAR(256),
-    blogger_id VARCHAR(256),
     comment_id VARCHAR(256),
-    date DATETIME,
-    content VARCHAR(256),
+    date VARCHAR,
+    content VARCHAR(15000),
     sentiment VARCHAR(256)
 );
 
